5790f542cd828df19802a361ad5750bffbcdbe83
geekgit <geeekz@gmail.com>
2016-01-18 02:57:12 +0500

    powershell build script update

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,14 +1,27 @@
 function get-csc
 {
 	$PathAL=New-Object System.Collections.ArrayList
+	#System
+	$PathAL.Add("$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\csc.exe") | Out-Null
+	$PathAL.Add("$env:SystemRoot\Framework\v4.0.30319\csc.exe") | Out-Null
 	#VS2015
-	$PathAL.Add("C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\csc.exe") | Out-Null
-	$PathAL.Add("C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe") | Out-Null
+    $PathAL.Add("$env:ProgramFiles (x86)\MSBuild\14.0\Bin\amd64\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles (x86)\MSBuild\14.0\Bin\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles\MSBuild\14.0\Bin\amd64\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles\MSBuild\14.0\Bin\csc.exe") | Out-Null
+	#VS2013
+	$PathAL.Add("$env:ProgramFiles (x86)\MSBuild\12.0\Bin\amd64\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles (x86)\MSBuild\12.0\Bin\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles\MSBuild\12.0\Bin\amd64\csc.exe") | Out-Null
+	$PathAL.Add("$env:ProgramFiles\MSBuild\12.0\Bin\csc.exe") | Out-Null
 	#
 	foreach($path in $PathAL)
 	{
 		$flag=(Test-Path $path)
-		return $path
+        if($flag)
+		{
+			return $path
+		}
 	}
 	return "N/A"
 }
